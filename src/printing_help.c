cc3e5d52587a5496bf94d34379b161324fe7d569
Steve Anderson <steve@bootladder.com>
2022-05-25 18:05:36 -0400

    log_debug_string is behind a #define guard

--- src/printing_help.c
+++ src/printing_help.c
@@ -1,6 +1,7 @@
 #include "printing_help.h"
 #include <stdio.h>
 
+#ifdef ATCOMMAND_DEBUG
 void printbuf(uint8_t * buf, int size){
 }
 
@@ -11,3 +12,14 @@ void log_debug_string(const char * a){}
 void log_debug_stringln(const char * a){
     printf(a);
 }
+
+#else
+
+void log_debug_string(const char * a){}
+void log_debug_stringln(const char * a){}
+
+void printbuf(uint8_t * buf, int size){}
+void printbuf_until_newline_or_zero(uint8_t * buf){}
+
+#endif
+
--- src/printing_help.h
+++ src/printing_help.h
@@ -3,12 +3,16 @@
 
 #include <stdint.h>
 
-#define PRINT_STRING(a) printbuf((uint8_t *)a , sizeof(a));
+#define NOT_ATCOMMAND_DEBUG
 
 void log_debug_string(const char * a);
 void log_debug_stringln(const char * a);
 
 void printbuf(uint8_t * buf, int size);
 void printbuf_until_newline_or_zero(uint8_t * buf);
+
+
+
+#define PRINT_STRING(a) printbuf((uint8_t *)a , sizeof(a));
 
 #endif //WRISTBAND_V2_5_BOOTLOADER_1_0_PRINTING_HELP_H
