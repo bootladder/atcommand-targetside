3ae8e511c366632d4171b33af3d7c406b1b400d9
Steve Anderson <steve@bootladder.com>
2022-08-05 09:46:57 -0400

    match longest command template because prefixes can match

--- src/AtCommandParser.c
+++ src/AtCommandParser.c
@@ -26,6 +26,9 @@ uint8_t AtCommandParser_AddCommandEntry(AtCommandHandler_t * handler){
 AtCommandHandler_t * AtCommandParser_ParseLine(uint8_t * line){
 
     // SCAN FOR A MATCH
+    AtCommandHandler_t * match = 0 ; // NULL
+    uint8_t longest_length_matched = 0;
+
     for(int i=0; i<NUM_ATCOMMANDHANDLERS; i++) {
 
         // skip null entries
@@ -33,15 +36,22 @@ AtCommandHandler_t * AtCommandParser_ParseLine(uint8_t * line){
             continue;
         }
 
+        // Some command templates may substrings of others.
+        // Thus, find the match with the longest length
+        uint8_t len_of_cmd_tmpl = strlen(atcommandhandlers[i]->command_template);
+
         if (0 == strncmp(
                 atcommandhandlers[i]->command_template,
                 line,
-                strlen(atcommandhandlers[i]->command_template))) {
-
-            return atcommandhandlers[i];
+                len_of_cmd_tmpl)) {
+            if(len_of_cmd_tmpl > longest_length_matched ){
+                match = atcommandhandlers[i];
+                longest_length_matched = len_of_cmd_tmpl;
+            }
         }
     }
-    return 0;  //NULL
+
+    return match;
 }
 
 void AtCommandParser_DeleteCommandHandlerTable(void){
